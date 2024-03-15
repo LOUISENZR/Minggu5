6f90dd0b5f89b7e4030bf958a50600aa9105a0be
LOUISENZR <louisanaza@gmail.com>
2024-03-15 21:24:30 +0700

    percobaan 1 pertanyaan 3

--- faktorial.java
+++ faktorial.java
@@ -5,11 +5,13 @@ public class faktorial {
  }
  public int nilai;
  public int faktorialBF(int n){
-   int fakto = 1 ;
-   for (int i=1; i<=n; i++){
-    fakto = fakto * i;
-   }
-   return fakto;
+    int fakto = 1 ;
+    int  i = 1;
+    while (i<=n){
+      fakto = fakto*i;;
+      i++;
+    }
+    return fakto;
  }
  public int faktorialDC(int n){
     if (n==1){
--- mainFaktorial.java
+++ mainFaktorial.java
@@ -2,9 +2,9 @@ package Pertemuan5;
 import java.util.Scanner;
 public class mainFaktorial {
 public static void main(String[] args) {
-    Scanner sc16 = new Scanner (System.in);=
+    Scanner sc16 = new Scanner (System.in);
     System.out.println("=========================================");
-    System.out.println("Masukan Elemen Yang Akan Dihitung : ");
+    System.out.print("Masukan Elemen Yang Akan Dihitung : ");
     int elemen = sc16.nextInt();
 
     faktorial[] fk = new faktorial[elemen];
@@ -16,12 +16,12 @@ public static void main(String[] args) {
     System.out.println("==========================================");
     System.out.println("Hasil Faktorial Dengan Brute Force");
     for (int i = 0; i< elemen; i++){
-        System.out.println("Faktorial Dari Nilai "+ fk[i].nilai+"adalah"+ fk[i].faktorialBF(fk[i].nilai));
+        System.out.println("Faktorial Dari Nilai "+ fk[i].nilai+" adalah "+ fk[i].faktorialBF(fk[i].nilai));
     }
     System.out.println("==========================================");
     System.out.println("Hasil Faktorial Dengan Brute Force");
     for (int i = 0; i< elemen; i++){
-        System.out.println("Faktorial Dari Nilai "+ fk[i].nilai+"adalah"+ fk[i].faktorialDC(fk[i].nilai));
+        System.out.println("Faktorial Dari Nilai "+ fk[i].nilai+" adalah "+ fk[i].faktorialDC(fk[i].nilai));
     }
     System.out.println("==========================================");
 }    
