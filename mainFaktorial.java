d6a685dd191d8ba7f1c5dc2a1530166b2ec1f8de
LOUISENZR <louisanaza@gmail.com>
2024-03-15 21:07:22 +0700

    add file

---
+++ mainFaktorial.java
@@ -0,0 +1,28 @@
+package Pertemuan5;
+import java.util.Scanner;
+public class mainFaktorial {
+public static void main(String[] args) {
+    Scanner sc16 = new Scanner (System.in);=
+    System.out.println("=========================================");
+    System.out.println("Masukan Elemen Yang Akan Dihitung : ");
+    int elemen = sc16.nextInt();
+
+    faktorial[] fk = new faktorial[elemen];
+    for (int i=0; i< elemen; i++){
+        fk[i] = new faktorial();
+        System.out.print("Masukan Nilai Data ke-" +(i+1)+" : ");
+        fk [i].nilai= sc16.nextInt(); 
+    }
+    System.out.println("==========================================");
+    System.out.println("Hasil Faktorial Dengan Brute Force");
+    for (int i = 0; i< elemen; i++){
+        System.out.println("Faktorial Dari Nilai "+ fk[i].nilai+"adalah"+ fk[i].faktorialBF(fk[i].nilai));
+    }
+    System.out.println("==========================================");
+    System.out.println("Hasil Faktorial Dengan Brute Force");
+    for (int i = 0; i< elemen; i++){
+        System.out.println("Faktorial Dari Nilai "+ fk[i].nilai+"adalah"+ fk[i].faktorialDC(fk[i].nilai));
+    }
+    System.out.println("==========================================");
+}    
+}
